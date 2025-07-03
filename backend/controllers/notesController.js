770a6d2de1b63c86e8ec8fcc743e9f716b6ea201
Erik <104572224+Erik-Sandqvist@users.noreply.github.com>
2025-07-03 22:23:21 +0200

    routes and controllers

---
+++ backend/controllers/notesController.js
@@ -0,0 +1,23 @@
+export async function getAllNotes  (req, res)  {   
+    res.send("you  got notes");
+      res.status(200).send("Here are your notes");
+}
+
+export async function getNoteById(req, res) {
+    const noteId = req.params.id;
+    res.status(200).json({ message: `Note with ID ${noteId} retrieved successfully` });
+}
+
+export async function createNote(req, res) {    
+    const newNote = req.body;
+    res.status(201).json({ message: "Note created successfully"});
+}
+
+export async function updateNote(req, res) {
+    res.status(200).json({ message: `Note updated successfully`});
+}
+
+export async function deleteNote(req, res) {
+    const noteId = req.params.id;
+    res.status(200).json({ message: `Note with ID ${noteId} deleted successfully` });
+}
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,11 @@
 import express from 'express';
+import notesRoutes from './routes/notesRoutes.js';
 // const express = require("express");
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    
-    res.send("you got notes");
-    res.status(200).send("Here are your notes");
-});
+app.use("/api/notes", notesRoutes); 
 
-app.get("/api/notes/:id", (req, res) => {
-});
-
-app.post("/api/notes", (req, res) => {
-res.status(201).send("Note created successfully");
-});
 
 app.listen(5001, () => {
   console.log('Server is running on http://localhost:5001');
